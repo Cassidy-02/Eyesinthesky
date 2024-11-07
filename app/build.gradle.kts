bcdbb23ff478c2fba8e62273910c7ca39e187453
Cassidy/02 <cassidymotto15@gmaul.com>
2024-11-07 12:28:30 +0000

    -Added dependencies for retrofit2, room and firebase realtime and authentication. -Updated the SDK as well meaning gradle versions updated.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
+
 }
 
 android {
@@ -42,7 +44,38 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation ("com.google.android.gms:play-services-maps:19.0.0")
+    implementation ("com.google.android.gms:play-services-location:21.3.0")
+    implementation(libs.androidx.espresso.core)
+    implementation(libs.firebase.firestore.ktx)
+    implementation(libs.play.services.tasks)
+    implementation(libs.play.services.basement)
+    implementation(libs.play.services.basement)
+    implementation(libs.play.services.basement)
+    implementation(libs.play.services.basement)
+    implementation(libs.play.services.basement)
+    implementation(libs.firebase.database.ktx)
+    implementation(libs.androidx.room.common)
+    implementation(libs.androidx.room.runtime.android)
+    implementation(libs.androidx.room.ktx)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("androidx.room:room-runtime:2.5.0")
+    annotationProcessor("androidx.room:room-compiler:2.5.0")
+    implementation("androidx.room:room-ktx:2.5.0")
+
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.0")
+    implementation (platform("com.google.firebase:firebase-bom:32.0.0"))
+
+    implementation ("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.android.gms:play-services-auth:21.2.0")
+    implementation ("com.google.firebase:firebase-auth:21.1.0")
+
+
 }
